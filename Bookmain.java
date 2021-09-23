5ec91e13432c83258673ef67e0b2c6fcaed96448
Eleni K <71977337+Lena341@users.noreply.github.com>
2021-09-23 20:21:18 +0300

    Add files via upload

---
+++ Bookmain.java
@@ -0,0 +1,10 @@
+package bookDatabase;
+
+public class Bookmain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        BookFrame title=new BookFrame("Books");
+	}
+
+}
